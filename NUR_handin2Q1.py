ab7a0f796041fcc3b70d3f7021fb3402a3468358
evanderkamp <124895640+evanderkamp@users.noreply.github.com>
2023-03-28 09:37:15 +0200

    Add files via upload

---
+++ NUR_handin2Q1.py
@@ -0,0 +1,331 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#simple trapezoid integration
+def trapezoid(N, x0, xmax, func):
+	"""Trapezoid integration with N steps, integrating func from x0 to xmax"""
+	h = (xmax-x0)/N
+	xes = np.linspace(x0,xmax,N)
+	integr = h*(func(xes[0])*0.5 + np.sum(func(xes[1:N-1])) + func(xes[N-1])*0.5)
+
+	return integr
+
+#Romberg integration
+def Romberg(N, m, x0, xmax, func):
+	"""Romberg integration with N steps, an order of m, integrating func from x0 to xmax"""
+	h = (xmax-x0)/N
+	r = np.zeros(m)
+	r[0] = trapezoid(N, x0, xmax, func)
+	Np = N
+	for i in range(1,m):
+		r[i] = 0
+		diff = h
+		h *= 0.5
+		x = x0+h
+
+		for k in range(Np):
+			r[i] += func(x)
+			x += diff
+
+		
+		r_i = r[i].copy()
+
+		r[i] = 0.5*(r[i-1]+diff*r_i)
+
+
+		Np *= 2
+
+
+
+	Np = 1
+	for i in range(1,m):
+		Np *= 4
+
+		for j in range(0,m-i):
+			r[j] = (Np*r[j+1] - r[j])/(Np-1)
+		    
+	return r[0]
+
+
+
+A=1
+Nsat=100
+a,b,c = 2.4,0.25,1.6
+
+
+
+def n2(x,a=2.4,b=0.25,c=1.6):
+	"""Returns n(x)*x^2 /(A*Nsat)"""
+	return  4*np.pi * (x**(a-1)/(b**(a-3)))*np.exp(-(x/b)**c)
+
+#we have that x = r/r_vir, so integrating over spherical coordinates gives dx = dr/r_vir, and n(x) doesnt depend on theta or phi so the 3D integral is simply 4pi times the integral over r^2*dr (or r_vir^3*x^2*dx)
+#To solve for A we can rewrite the integral to get (4pi/(A*N_sat))*int n(x)*x^2 *dx = 1/(A*r_vir^3)
+#a < 3 so we're dividing by zero if x=0, so put the x**2 into it
+
+A_inv = Romberg(50,6,0,5,n2)
+A_intgr = 1/A_inv
+
+print("The result from integration with 50 steps and order 6:", A_inv)
+print("And A*r_vir^3", A_intgr)
+
+np.savetxt("NUR2Q1Asol.txt", [A_intgr])
+
+#b
+#N(x)dx is 4*pi*n(x)*x^2 dx, because integrating that divided by Nsat gives 1, so integrating p(x)dx also gives 1, which is what we want
+
+def N(x,A=A_intgr,a=2.4,b=0.25,c=1.6):
+	"""Returns N(x)dx/Nsat"""
+	return 4*np.pi* A*(x**(a-1)/(b**(a-3)))*np.exp(-(x/b)**c)
+
+def LCG64(I0,size, a=1664525, m=2**32, c=1013904223, a1=21, a2=25, a3=4):
+    """Random number generator using 64-bit XOR and LCG, gives back an array of random numbers between 0 and 1 of size size with I0 as seed."""
+    rndnrs = np.zeros(size,dtype=int)
+    xors = np.zeros(size+1,dtype=int)
+    xors[0] = int(I0)
+    
+    for i in range(1,size+1):
+        x0 = xors[i-1]
+        x1 = x0 ^ (x0 >> a1)
+        x2 = x1 ^ (x1 << a2)
+        x3 = x2 ^ (x2 >> a3)
+        
+        xors[i] = x3
+        
+    for k in range(0,size):
+    
+        rndnrs[k] = (a*xors[k+1] + c)%m
+        
+        #return between 0 and 1
+    return rndnrs/np.amax(rndnrs)
+
+
+
+def RejectSamp(prob, a, b, N):
+	"""Rejection sampling N points with a probability prob from x = [a,b]"""
+	sample = np.zeros(N)
+	i=0
+	j=0 
+	x_range = np.linspace(a,b,N)
+	probs = prob(x_range)
+	maxprob = np.amax(probs)
+	while i < N:
+		#make sure that the seed changes every time so we get a different number
+		rndnr = LCG64(I0=(2*j+N+12), size=100)
+		x = a + (b-a)*rndnr[0] #U(a,b)
+		y = maxprob*rndnr[1] #y goes from zero to max(p(x))
+		
+		if y <= prob(x):
+			sample[i] = x
+			i += 1
+			j += 1
+		else:
+			#reject but still take another seed
+			j += 1
+
+	return sample
+
+x_sample = RejectSamp(N, a=0, b=5, N=10000)
+
+xes = np.linspace(0,5,1000)
+
+#setting density=True gives a probability density distribution by dividing the bins by their width and ans the total number of counts
+
+#the bins below 5*10**(-3) are empty because there the probability reaches ~10^(-4) so with 10000 points you expect at most 1 x there
+plt.hist(x_sample, bins=np.logspace(np.log10(10**(-4)),np.log10(5), 20), density=True, rwidth=0.9, label='histogram of sampled x')
+plt.xlabel('x')
+
+plt.loglog(xes,N(xes), label='p(x)')
+plt.legend()
+plt.savefig("NUR2Q1plot1.pdf")
+plt.close()
+
+
+#c
+
+def rowswapvec(M, i, j):
+	"""swap indices i and j of a vector M"""
+	B = np.copy(M).astype("float64")
+
+	row = B[i].copy()
+	B[i] = B[j]
+	B[j] = row
+	return B
+
+def Quicksort_part(arr, ind_first, ind_last, indsave, indices):
+	"""Sorts array arr from ind_first to ind_last by taking a pivot and sorting it with respect to the pivot"""
+	#arr is the full array, a is the part of the array we're sorting
+	a = arr[ind_first:ind_last+1].copy()
+	Ntot = len(arr)
+	N = len(a)
+	middle = int(N*0.5)
+    	#if N < 3, then we dont have to sort
+	if N < 3:
+		return arr, indices
+    	#pivot in the middle
+	x_piv = a[middle]
+
+	i = ind_first
+	j = ind_last
+	i_2 = 0
+	j_2 = N-1
+	#sort arr (which we return) based on a, and sort a too
+	for k in range(0,N):
+		if i >= j:
+	    		break
+		if arr[i] >= x_piv:
+			pass
+		else:
+			i += 1
+			i_2 += 1
+		if arr[j] <= x_piv:
+			pass
+		else:
+			j -= 1
+			j_2 -= 1
+		if arr[i] >= x_piv and arr[j] <= x_piv:
+			arr = rowswapvec(arr,i,j)
+			a = rowswapvec(a,i_2,j_2)
+			if indsave:
+				indices = rowswapvec(indices,i,j)
+                        
+	#sort left and right of the pivot
+
+	first = a[0]
+	last = a[N-1]
+
+	indxes = np.arange(0,Ntot,1, dtype=int)
+	indxes2 = np.arange(0,N,1, dtype=int)
+	#get the indices of the pivot in both arr and a
+	indx_piv = (indxes[arr == x_piv])[0]
+	indx_piv2 = (indxes2[a == x_piv])[0]
+	in_first = (indxes[arr == first])[0]
+	in_last = (indxes[arr == last])[0]
+	
+	#if the pivot is at the edge, only sort right or left of it, otherwise, sort both sides
+	if indx_piv2 <= 1:
+		arr, indices = Quicksort_part(arr, indx_piv+1, in_last, indsave, indices)
+	if indx_piv2 >= N-2:
+		arr, indices = Quicksort_part(arr, in_first, indx_piv, indsave, indices)
+	else:
+		arr, indices = Quicksort_part(arr, in_first, indx_piv, indsave, indices)
+		arr, indices = Quicksort_part(arr, indx_piv+1, in_last, indsave, indices)
+        
+
+	return arr, indices
+
+ 
+            
+def Quicksort(arr, indsave=False):
+	"""Sorts array arr using the Quicksort algorithm, if indsave is true, we give back the indices of the sorted array instead of the sorted array itself."""
+	a = arr.copy()
+	N = len(arr)
+	middle = int(N*0.5)
+	#make an array of indices to keep track of
+	indxes = np.arange(0,N,1, dtype=int)
+    	#first sort the first middle and last of the array
+	fml = np.array([a[0], a[middle], a[N-1]]).copy()
+	fml_ind = np.array([0,middle,N-1])
+	a[0], indxes[0] = np.amin(fml), fml_ind[fml == np.amin(fml)][0]
+        
+	a[N-1], indxes[N-1] = np.amax(fml), fml_ind[fml == np.amax(fml)][0]
+
+
+	if fml[(fml > a[0]) & (fml < a[N-1])].size == 0: 
+        	#this means that there are 2 equal numbers 
+		pass
+	else:
+		a[middle] = fml[(fml > a[0]) & (fml < a[N-1])]
+		indxes[middle] = fml_ind[fml == fml[(fml > a[0]) & (fml < a[N-1])]]
+    
+    
+
+	in_first = 0
+	in_last = N-1
+
+	#sort the rest
+	a, indxes = Quicksort_part(a, in_first, in_last, indsave, indxes)        
+
+
+	if indsave:
+		return indxes
+    
+	return a
+        
+#I will draw 100 random galaxies by shuffling the sample array and taking the first 100 of the shuffled array
+#shuffle a random array by sorting it and getting the indices
+rand_arr = LCG64(I0=5, size=len(x_sample))
+rand_ind = Quicksort(rand_arr, indsave=True)
+
+rand_sample = x_sample[rand_ind.astype(int)][0:100]
+#sort the random 100 galaxies
+sort_sample = Quicksort(rand_sample)
+
+print("Sorted 100 random galaxies", sort_sample)
+
+#to get the number within a certain radius, we can use the sorted sample
+#there are 100 samples so counts go from 0 to 100
+counts = np.arange(0,101,1)
+counts_fin = np.append(counts,100)
+
+
+xes_samp = np.insert(sort_sample, 0, 10**(-4))
+xes_samp = np.insert(xes_samp, len(xes_samp), 5)
+
+plt.plot(xes_samp, counts_fin)
+plt.xscale('log')
+plt.xlabel("r/r_vir")
+plt.ylabel("number of haloes with r < r/r_vir")
+plt.savefig("NUR2Q1plot2.pdf")
+plt.close()
+
+
+#d
+def centr_diff(func, x, h):
+    """calculates the derivative with the central difference method"""
+    return (func(x+h) - func(x-h))/(2*h)
+
+def Ridder(func, x, h, d, accur, analder, m=100):
+    """calculates the derivative of func at x with stepsize h, order d, up until accuracy accur"""
+    r = np.zeros((m, len(x)))
+    r[0,:] = centr_diff(func,x,h)
+    d_inv = 1/d
+    
+    for i in range(1,m):
+        h *= d_inv
+        r[i,:] = centr_diff(func,x,h)
+        
+    
+    
+    Np = 1
+    for i in range(1,m):
+        Np *= d**2
+        
+        accurprev = np.mean(np.abs(r[0,:] - analder(x)))
+        
+        for j in range(0,m-i):
+            r[j,:] = (Np*r[j+1,:] - r[j,:])/(Np-1)
+            
+        accurnow = np.mean(np.abs(r[0,:] - analder(x)))
+        if accurnow > accurprev:
+            print("error grew", accurprev, accurnow)
+            return r[0,:]
+        if accurnow < accur:
+            print("Number of steps taken, error:", i, np.mean(np.abs(r[0,:] - analder(x))))
+            return r[0,:]
+            
+    return r[0,:]
+
+def n(x,A=A_intgr,Nsat=100, a=2.4,b=0.25,c=1.6):
+	"""Returns n(x)"""
+	return Nsat* A*((x/b)**(a-3))*np.exp(-(x/b)**c)
+
+def dn(x,A=A_intgr,Nsat=100, a=2.4,b=0.25,c=1.6):
+	"""Returns dn(x)/dx"""
+	return Nsat* A*((x)**(a-4)/(b)**(a-3))*np.exp(-(x/b)**c)*(a-3-c*(x/b)**c)
+
+xje = np.ones(1)
+analderiv, Ridderderiv = dn(1), Ridder(n,xje,0.1,2,accur=10**(-8),analder=dn)
+print("Analytical derivative at x=1, and via Ridder's method:", analderiv, Ridderderiv)
+
+np.savetxt("NUR2Q1derivs.txt", [analderiv, Ridderderiv])
+
---
+++ NUR_handin2Q2.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import timeit
+
+
+k=1.38e-16 # erg/K
+aB = 2e-13 # cm^3 / s
+
+#a
+
+# here no need for nH nor ne as they cancel out
+def equilibrium1(T,Z=0.015,Tc=10**4,psi=0.929):
+	"""Returns Gamma_pe - Lambda_rr, divided by nH, ne, alphaB because those appear in both expressions"""
+	return psi*Tc*k - (0.684 - 0.0416 * np.log(T/(1e4 * Z*Z)))*T*k
+
+
+
+def NR(func, deriv, c, max_iter, accur):
+	"""Uses the Newton-Rhapson method to find the root of func, using the initial guess c and the derivative deriv. It iterates either max_iter times or until f(c) < accur."""
+	for i in range(0, max_iter):       
+        
+        
+		c = c - func(c)/deriv(c)
+        
+		print("Step ", i+1, "f(c) =", func(c))
+        
+		if np.abs(func(c)) < accur:
+			return c
+        
+	return c
+
+
+
+def equilder1(T,Z=0.015):
+	"""Returns the derivative of Gamma_pe - Lambda_rr, divided by nH, ne, alphaB because those appear in both expressions"""
+	return - (0.684 - 0.0416 * np.log(T/(1e4 * Z*Z)) - 0.0416)*k
+
+
+#We want an accuracy of at least 0.1K, but the function is around zero for a long time so we want a high accuracy
+#Secant diverges, so I use Newton-Rhapson with 10**3.5 as initial guess
+
+starttime = timeit.default_timer()
+T_equil = NR(equilibrium1, equilder1, 10**3.5, 30, 10**(-15))
+timetaken = timeit.default_timer() - starttime
+print("Time taken with NR", timetaken, " s")
+print("T_equil = ", T_equil)
+np.savetxt("NUR2Q2TandttNR.txt", [T_equil, timetaken])
+
+#plotting to show that the T is found within 0.1K
+T_arr = np.linspace(1,10**7,10000000)
+zeroes = np.zeros(10000000)
+plt.plot(T_arr,equilibrium1(T_arr), label='equilibrium function')
+plt.plot(T_arr, zeroes, label='zero')
+plt.scatter(T_equil, equilibrium1(T_equil), label='root by NR')
+plt.xscale('log')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Equilibrium function")
+#set xlimits 0.1K around the found equilibrium to see how close we are
+plt.xlim(T_equil-1e-1, T_equil+1e-1)
+plt.ylim(1e-18, -1e-18)
+plt.legend()
+plt.savefig("NUR2Q2plot1.pdf")
+plt.close()
+
+
+
+#nH = ne so we can divide out one
+def equilibrium2(T, Z=0.015,Tc=10**4,psi=0.929,  A=5*10**(-10), xi=10**(-15)):
+	"""Returns Gamma_pe + Gamma_CR + Gamma_MHD - Lambda_rr - Lambda_FF, divided by ne, because that appear in all expressions"""
+	return (psi*Tc - (0.684 - 0.0416 * np.log(T/(1e4 * Z*Z)))*T - .54 * ( T/1e4 )**.37 * T)*k*nH*aB + A*xi + 8.9e-26 * (T/1e4)
+
+#derivative
+def equilder2(T, Z=0.015,Tc=10**4,psi=0.929,  A=5*10**(-10), xi=10**(-15)):
+	"""Returns the derivative of Gamma_pe + Gamma_CR + Gamma_MHD - Lambda_rr - Lambda_FF, divided by ne, because that appear in all expressions"""
+	return ( - (0.684 - 0.0416 * np.log(T/(1e4 * Z*Z)) - 0.0416) - .54 * 1.37 * ( T/1e4 )**.37)*k*nH*aB + 8.9e-26 * (1/1e4)
+
+
+def Bisection(func, a, b, max_iter, accur):
+    """Finds the root of func with bisection between a and b, doing either max_iter steps or until f(c) < accur"""
+    if func(a) * func(b) > 0:
+        print("invalid!")
+        return 0
+    
+    c = (a+b)*0.5 
+    
+    for i in range(0, max_iter):
+        print("step", i, "accur =", func(c))
+        
+        if np.abs(func(c)) < accur:
+            return c
+    
+        if func(c) * func(b) < 0:
+            a = c
+        elif func(c) * func(a) < 0:
+            b = c
+        else:
+            print("can't find root")
+            return 0 
+        
+        c = (a+b)*0.5 
+        
+    return c
+
+
+def Secant(func, a, b, max_iter, accur):
+    """Finds the root of func with secant between a and b, doing either max_iter steps or until f(c) < accur"""
+    if func(a) * func(b) > 0:
+        print("invalid!")
+        return 0
+    
+
+    for i in range(0, max_iter):
+        
+        c = b + func(b)*(b-a)/(func(a) - func(b))
+        a,b = b,c
+        
+        print(i, "accur =", func(c))
+        
+        if np.abs(func(c)) < accur:
+            return c
+        
+    return c
+
+
+T_arr2 = np.linspace(1,10**15, 10**7)
+
+nH = 1e-4
+#for nH 1e-4 NR diverges and its really steep near the zero, so go use bisection
+starttime2_1 = timeit.default_timer()
+T_bisequil2_1 = Bisection(equilibrium2, 1, 10**15, 30, 10**(-17))
+timetaken2_1 = timeit.default_timer() - starttime2_1
+print("Time taken with Bisection", timetaken2_1, " s")
+print("T_bisequil = ", T_bisequil2_1)
+np.savetxt("NUR2Q2TandttBis.txt", [T_bisequil2_1, timetaken2_1])
+
+plt.plot(T_arr2,equilibrium2(T_arr2), label='equilibrium function')
+plt.plot(T_arr2, zeroes, label='zero')
+plt.scatter(T_bisequil2_1, equilibrium2(T_bisequil2_1), label='root by bis')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Equilibrium function")
+plt.xscale('log')
+plt.savefig("NUR2Q2plot2.pdf")
+plt.close()
+
+#with nH >=1 it is almost always > 0 except at T=1, so use Secant
+nH = 1
+starttime2_2 = timeit.default_timer()
+T_bisequil2_2 = Secant(equilibrium2, 1, 10**15, 30, 10**(-17))
+timetaken2_2 = timeit.default_timer() - starttime2_2
+print("Time taken with Secant", timetaken2_2, " s")
+print("T_equil = ", T_bisequil2_2)
+np.savetxt("NUR2Q2TandttSec.txt", [T_bisequil2_2, timetaken2_2])
+
+plt.plot(T_arr2,equilibrium2(T_arr2), label='equilibrium function')
+plt.plot(T_arr2, zeroes, label='zero')
+plt.scatter(T_bisequil2_2, equilibrium2(T_bisequil2_2), label='root by secant')
+plt.xscale('log')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Equilibrium function")
+plt.xscale('log')
+plt.savefig("NUR2Q2plot3.pdf")
+plt.close()
+
+
+
+nH = 1e4
+starttime2_3 = timeit.default_timer()
+T_bisequil2_3 = Secant(equilibrium2, 1, 10**15, 30, 10**(-17))
+timetaken2_3 =  timeit.default_timer() - starttime2_3
+print("Time taken with Secant", timetaken2_3, " s")
+
+print("T_equil = ", T_bisequil2_3)
+np.savetxt("NUR2Q2TandttSec2.txt", [T_bisequil2_3, timetaken2_3])
+
+plt.plot(T_arr2,equilibrium2(T_arr2), label='equilibrium function')
+plt.plot(T_arr2, zeroes, label='zero')
+plt.scatter(T_bisequil2_3, equilibrium2(T_bisequil2_3), label='root by secant')
+plt.xscale('log')
+plt.xlabel("Temperature (K)")
+plt.ylabel("Equilibrium function")
+plt.xscale('log')
+plt.savefig("NUR2Q2plot4.pdf")
+plt.close()
+
+
